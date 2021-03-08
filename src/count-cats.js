00355f760320cdd784b68ebdfa49a04f68ff9b6a
burletov <burletov@bk.ru>
2021-03-08 19:11:38 +0300

    feat: implement five functions

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,6 +1,5 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function countCats(/* matrix */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function countCats(matrix) {
+  return matrix.map(el => el.filter(el => el === '^^')).reduce((sum, el) => sum + el.length, 0)
 };
