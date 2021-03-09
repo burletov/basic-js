27dae1b0acd83d92b391c6e23d4f769bb98ca73b
burletov <burletov@bk.ru>
2021-03-09 21:12:31 +0300

    feat: implement 3 functions

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,17 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function transform(arrayOne) {
+  if (typeof arrayOne !== "object") throw Error;
+  let arrayTwo = [];
+  for (let i = 0; i < arrayOne.length; i++) {
+    if (arrayOne[i + 1] === "--discard-prev" || arrayOne[i] === "--discard-prev") {
+    } else if (arrayOne[i] === "--discard-next") i++;
+    else if (arrayOne[i] === "--double-next") {
+      if (arrayOne[i + 1] !== undefined) arrayTwo.push(arrayOne[i + 1]);
+    } else if (arrayOne[i] === "--double-prev") {
+      if (arrayOne[i - 2] === "--discard-next") {
+      } else if (arrayOne[i - 1] !== undefined) arrayTwo.push(arrayOne[i - 1]);
+    } else arrayTwo.push(arrayOne[i]);
+  }
+  return arrayTwo;
 };
